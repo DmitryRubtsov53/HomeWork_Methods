4a86cea05165add7f9839f7fee480a366d71b018
DmitryRubtsov53 <2578390@mail.ru>
2022-09-23 22:18:10 +0500

    Решил Задачу 1 Урока Методы.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашняя работа Урока Методы ************************************* ");
+        System.out.println();
+
+        System.out.println("Задача 1. Високосный год.");
+        // Нужно определить является текущий год високосным или нет. Determine leap year.
+        int year = 2022; // метод определения номера текущего года
+        determineLeapYear(year);
+
+
+        System.out.println(); //***********current*********************************************************
+
+    }
+
+
+
+    public static void determineLeapYear (int year) {  // определяем високосный год - determine leap year
+
+         if (year % 4 == 0) {
+            if ((year % 100 == 0) && (year % 400 == 0)) {
+                System.out.println(year + " год является високосным.");
+            }
+        } else System.out.println(year + " год не является високосным.");
+
+
     }
 }
