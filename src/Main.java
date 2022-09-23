d7c90b3e6e6678c11e372a46379c55b6c6bb35f4
DmitryRubtsov53 <2578390@mail.ru>
2022-09-23 23:15:19 +0500

    Решил Задачи 1 и 2 Урока Методы.

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,58 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашняя работа Урока Методы ************************************* ");
+        System.out.println("Домашняя работа Урока Методы");
         System.out.println();
 
-        System.out.println("Задача 1. Високосный год.");
+        System.out.println("Задача 1. Високосный год *******************************************************");
         // Нужно определить является текущий год високосным или нет. Determine leap year.
-        int year = 2022; // метод определения номера текущего года
-        determineLeapYear(year);
+        int currentYear = LocalDate.now().getYear();; // метод определения номера текущего года
+        determineLeapYear(currentYear);
+
+    System.out.println();
+        System.out.println("Задача 2. Помощь в выборе клиентом приложения для смартфона ********************");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите тип операционной системы своего смартфона:");
+        System.out.println("( если у вас iOS - 0, если Adroid - 1 )");
+        int osType = scanner.nextInt(); // тип операционной системы смартфона клиента
+        System.out.println("Введите год выпуска вашего смартфона:");
+        int yearOfIssue = scanner.nextInt();   // год выпускв
+
+        selectionOS (osType, yearOfIssue);
 
 
-        System.out.println(); //***********current*********************************************************
-
-    }
+    }  // скобка метода main
 
 
-
-    public static void determineLeapYear (int year) {  // определяем високосный год - determine leap year
-
-         if (year % 4 == 0) {
-            if ((year % 100 == 0) && (year % 400 == 0)) {
-                System.out.println(year + " год является високосным.");
+    // ********* мой методыы ********************************************************************
+    public static void determineLeapYear (int currentYear) {  // определяем високосный год - determine leap year
+         if (currentYear % 4 == 0) {
+            if ((currentYear % 100 == 0) && (currentYear % 400 == 0)) {
+                System.out.println(currentYear + " год является високосным.");
             }
-        } else System.out.println(year + " год не является високосным.");
-
-
+        } else System.out.println(currentYear + " год не является високосным.");
     }
-}
+
+    public static void selectionOS (int osType, int yearOfIssue) {
+        int currentYear = LocalDate.now().getYear();; // метод определения номера текущего года
+
+        if (yearOfIssue < currentYear && osType == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        } else
+        if (yearOfIssue < currentYear && osType == 1) {
+            System.out.println("Установите облегченную  версию приложения для Android по ссылке.");  /// Здесь наставник использовал if - else if
+        } else
+        if (yearOfIssue >= currentYear && osType == 0) {
+            System.out.println("Установите приложение для iOS по ссылке.");
+        } else
+        if (yearOfIssue >= currentYear && osType == 1) {
+            System.out.println("Установите приложение для Android по ссылке.");
+        }
+    }
+
+
+
+}   // скобка класса
