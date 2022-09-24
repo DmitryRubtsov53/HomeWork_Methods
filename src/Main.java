88847260a77a7f71e417ad379d3f64369bad7bb4
DmitryRubtsov53 <2578390@mail.ru>
2022-09-24 08:11:42 +0500

    Решил все обязательные Задачи (№ 1,2,3) Урока Методы.

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         int currentYear = LocalDate.now().getYear();; // метод определения номера текущего года
         determineLeapYear(currentYear);
 
-    System.out.println();
+        System.out.println();
         System.out.println("Задача 2. Помощь в выборе клиентом приложения для смартфона ********************");
 
         Scanner scanner = new Scanner(System.in);
@@ -22,13 +22,29 @@ public class Main {
         int yearOfIssue = scanner.nextInt();   // год выпускв
 
         selectionOS (osType, yearOfIssue);
+        System.out.println();
 
+        System.out.println("Задача 3. Доставка банковских карт **********************************************");
+        System.out.println("Введите растояние до вас в км:");
 
+        int deliveryDistance = scanner.nextInt();
+        int deliveryTime = determineDeliveryTime (deliveryDistance);  // мой метод
+
+        switch (deliveryTime) {
+            case 1:
+                System.out.println("На доставку карты потребуется 1 день.");  break;
+            case 2:
+                System.out.println("На доставку карты потребуется 2 дня.");  break;
+            case 3:
+                System.out.println("На доставку карты потребуется 3 дня.");  break;
+            case 4:
+                System.out.println("Доставку карт на такое расстояние не делаем."); break;
+        }
     }  // скобка метода main
 
 
     // ********* мой методыы ********************************************************************
-    public static void determineLeapYear (int currentYear) {  // определяем високосный год - determine leap year
+    public static void determineLeapYear (int currentYear) {  // определяем високосный год - к Задаче 1.
          if (currentYear % 4 == 0) {
             if ((currentYear % 100 == 0) && (currentYear % 400 == 0)) {
                 System.out.println(currentYear + " год является високосным.");
@@ -36,22 +52,35 @@ public class Main {
         } else System.out.println(currentYear + " год не является високосным.");
     }
 
-    public static void selectionOS (int osType, int yearOfIssue) {
+    public static void selectionOS (int osType, int yearOfIssue) {   // к Задаче 2.
         int currentYear = LocalDate.now().getYear();; // метод определения номера текущего года
 
         if (yearOfIssue < currentYear && osType == 0) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            System.out.println("Установите lite версию приложения для iOS по ссылке.");
         } else
         if (yearOfIssue < currentYear && osType == 1) {
-            System.out.println("Установите облегченную  версию приложения для Android по ссылке.");  /// Здесь наставник использовал if - else if
+            System.out.println("Установите lite  версию приложения для Android по ссылке.");  /// Здесь наставник использовал if - else if
         } else
         if (yearOfIssue >= currentYear && osType == 0) {
-            System.out.println("Установите приложение для iOS по ссылке.");
+            System.out.println("Установите обычную версию приложения для iOS по ссылке.");
         } else
         if (yearOfIssue >= currentYear && osType == 1) {
-            System.out.println("Установите приложение для Android по ссылке.");
+            System.out.println("Установите обычную версию приложения для Android по ссылке.");
         }
     }
+
+    public static int determineDeliveryTime (int deliveryDistance) {  // к Задаче 3.
+
+        if (deliveryDistance <= 20) {
+            return 1;
+        } else if (20 < deliveryDistance && deliveryDistance <= 60) {
+            return 2;
+        } else if (60 < deliveryDistance && deliveryDistance <= 100) {
+            return 3;
+            }
+        return 4;
+    }
+
 
 
 
